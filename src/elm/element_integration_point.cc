bf351c4fa88e23d0804623daf533aad0d063e949
christophsusen <christoph.susen@rwth-aachen.de>
2018-05-11 12:46:07 +0200

    Add element_integration_point class to remove the 2d vector in parameter_space.

---
+++ src/elm/element_integration_point.cc
@@ -0,0 +1,32 @@
+/* Copyright 2018 Chair for Computational Analysis of Technical Systems, RWTH Aachen University
+
+This file is part of SplineLib.
+
+SplineLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3 of the License.
+
+SplineLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with SplineLib.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+#include "element_integration_point.h"
+
+#include <vector>
+
+elm::ElementIntegrationPoint::ElementIntegrationPoint(const std::vector<double> &non_zero_basis_functions)
+    : non_zero_basis_functions_(non_zero_basis_functions) {}
+
+std::vector<double> elm::ElementIntegrationPoint::non_zero_basis_functions() {
+  return non_zero_basis_functions_:
+}
+
+double elm::ElementIntegrationPoint::GetBasisFunctionValue(int firstNonZeroOffset) {
+#ifdef DEBUG
+  return non_zero_basis_functions_.at(firstNonZeroOffset);
+#else
+  return non_zero_basis_functions_[firstNonZeroOffset];
+#endif
+}
