5df804477139b9affd60e2b0af56d8b070f6e794
Alexander Jodlauk <jodlauk@hilbert.cats.rwth-aachen.de>
2018-05-22 13:09:23 +0200

    * removed some comments to solve the issue 34

--- test/multihandler_test.cc
+++ test/multihandler_test.cc
@@ -23,16 +23,10 @@ class MultiHandlers : public Test {
  public:
   MultiHandlers() {
     std::array<int, 1> lastKnotOffset1D = {10};
-    //currentIndex[0] = 10;
     multiIndexHandler1D = std::make_unique<util::MultiIndexHandler<1>>(lastKnotOffset1D);
     std::array<int, 2> lastKnotOffset2D = {10, 3};
-    //lastKnotOffset2D[0] = 10;
-    //lastKnotOffset2D[1] = 3;
     multiIndexHandler2D = std::make_unique<util::MultiIndexHandler<2>>(lastKnotOffset2D);
     std::array<int, 3> lastKnotOffset3D = {4, 3, 5};
-    //lastKnotOffset3D[0] = 4;
-    //lastKnotOffset3D[1] = 3;
-    //lastKnotOffset3D[2] = 5;
     multiIndexHandler3D = std::make_unique<util::MultiIndexHandler<3>>(lastKnotOffset3D);
   }
  protected:
@@ -47,7 +41,6 @@ TEST_F(MultiHandlers, 1D_multiIndex_ind0) {
 
 TEST_F(MultiHandlers, 1D_multiIndex_ind5) {
   std::array<int, 1> currentIndex = {5};
-  //currentIndex[0] = 5;
   multiIndexHandler1D->SetIndices(currentIndex);
   ASSERT_THAT(multiIndexHandler1D->Get1DIndex(), Eq(5));
 }
@@ -58,8 +51,6 @@ TEST_F(MultiHandlers, 2D_multiIndex_ind0) {
 
 TEST_F(MultiHandlers, 2D_multiIndex_ind3_2) {
   std::array<int, 2> currentIndex = {3, 2};
-  //currentIndex[0] = 3;
-  //currentIndex[1] = 2;
   multiIndexHandler2D->SetIndices(currentIndex);
   ASSERT_THAT(multiIndexHandler2D->Get1DIndex(), Eq(23));
 }
@@ -70,9 +61,6 @@ TEST_F(MultiHandlers, 3D_multiIndex_ind0) {
 
 TEST_F(MultiHandlers, 3D_multiIndex_ind2_1_2) {
   std::array<int, 3> currentIndex = {2, 1, 2};
-  //currentIndex[0] = 2;
-  //currentIndex[1] = 1;
-  //currentIndex[2] = 2;
   multiIndexHandler3D->SetIndices(currentIndex);
   ASSERT_THAT(multiIndexHandler3D->Get1DIndex(), Eq(30));
 }
