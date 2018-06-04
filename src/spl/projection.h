cb7f14f8df0c69fcbace895ace97030d7fd99a61
christophsusen <38278194+christophsusen@users.noreply.github.com>
2018-06-04 13:34:04 +0200

    Issue16 projection (#54)
    * Implement projection (not tested).
    
    * Add test (not working).
    
    * Rename test class.
    
    * A
    
    * Add first order projection algorithm (test working, 2nd order still not working).
    
    * Implement FindInitialValue method for spline projection.
    
    * NOT WORKING 2ND ORDER ALGORITHM.
    
    * Delete non-working second order algorithm.
    
    * Change input parameter of projection method from BSpline to Spline.
    
    * Reformat and rearrange code.
    
    * Add tests right and left of the outer knots to improve code coverage.

---
+++ src/spl/projection.h
@@ -0,0 +1,112 @@
+/* Copyright 2018 Chair for Computational Analysis of Technical Systems, RWTH Aachen University
+
+This file is part of SplineLib.
+
+SplineLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3 of the License.
+
+SplineLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with SplineLib.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SPLINELIB_PROJECTION_H
+#define SPLINELIB_PROJECTION_H
+
+#include <vector>
+#include <iostream>
+
+#include "b_spline.h"
+#include "numeric_settings.h"
+
+namespace spl {
+template<int DIM>
+class Projection {
+ public:
+  static std::array<double, DIM> ProjectionOnSpline(std::vector<double> pointPhysicalCoords,
+                                              spl::Spline<DIM> *spline) {
+    double kappa;
+    double tolerance = 0.0001;
+    int iteration = 0;
+    double distance;
+    double delta;
+    int signum;
+    std::vector<int> dimensions;
+    for (int i = 0; i < pointPhysicalCoords.size(); ++i) {
+      dimensions.emplace_back(i);
+    }
+    std::array<double, DIM> projectionPointParamCoords = FindInitialValue(pointPhysicalCoords, spline, dimensions);
+    bool converged = false;
+
+    while (not converged) {
+      if (DIM == 1) {
+        std::vector<double> firstDer = spline->EvaluateDerivative(projectionPointParamCoords, dimensions, {1});
+        std::vector<double> secondDer = spline->EvaluateDerivative(projectionPointParamCoords, dimensions, {2});
+        kappa = ComputeArea(firstDer, secondDer) / pow(ComputeTwoNorm(firstDer), 3);
+        std::vector<double> projectionVector = ComputePiecewiseVectorDifference(pointPhysicalCoords, spline->Evaluate(projectionPointParamCoords, dimensions));
+
+        //This is only the first order algorithm.
+        //An implemented but non-working version of the second order algorithm can be found in commit 2ed993e6dcef3d184b70640f6b9498efae52747a.
+        delta = ComputeScalarProduct(firstDer, projectionVector) / ComputeScalarProduct(firstDer, firstDer);
+        signum = 1;
+
+        projectionPointParamCoords[0] += signum * delta;
+        if (projectionPointParamCoords[0] < spline->GetKnotVector(0).knot(0)) {
+          projectionPointParamCoords[0] = spline->GetKnotVector(0).knot(0);
+        } else if (projectionPointParamCoords[0] > spline->GetKnotVector(0).GetLastKnot()) {
+          projectionPointParamCoords[0] = spline->GetKnotVector(0).GetLastKnot();
+        }
+        if (abs(delta) < tolerance) {
+          converged = true;
+        }
+      }
+      ++iteration;
+      if (iteration > 1000) {
+        break;
+      }
+    }
+    return projectionPointParamCoords;
+  }
+
+  static std::array<double, DIM> FindInitialValue(std::vector<double> pointPhysicalCoords,
+                                              spl::Spline<DIM> *spline, const std::vector<int> &dimensions) {
+    std::vector<elm::Element> elements = spline->GetElementList();
+    std::array<double, DIM> paramCoords;
+    std::vector<double> splinePhysicalCoords = spline->Evaluate({(0.5 * (elements[0].node(1) - elements[0].node(0)))}, dimensions);
+    double distance = ComputeTwoNorm(ComputePiecewiseVectorDifference(pointPhysicalCoords, splinePhysicalCoords));
+    paramCoords = {0.5 * (elements[0].node(1) - elements[0].node(0))};
+    for (int i = 1; i < elements.size(); ++i) {
+      splinePhysicalCoords = spline->Evaluate({0.5 * (elements[i].node(1) - elements[i].node(0)) + elements[i].node(0)}, dimensions);
+      if (ComputeTwoNorm(ComputePiecewiseVectorDifference(pointPhysicalCoords, splinePhysicalCoords)) < distance) {
+        distance = ComputeTwoNorm(ComputePiecewiseVectorDifference(pointPhysicalCoords, splinePhysicalCoords));
+        paramCoords = {0.5 * (elements[i].node(1) - elements[i].node(0)) + elements[i].node(0)};
+      }
+    }
+    return paramCoords;
+  }
+
+  static double ComputeTwoNorm(std::vector<double> vectorA) {
+    std::transform(vectorA.begin(), vectorA.end(), vectorA.begin(), vectorA.begin(), std::multiplies<double>());
+    return sqrt(std::accumulate(vectorA.begin(), vectorA.end(), 0));
+  }
+
+  static std::vector<double> ComputePiecewiseVectorDifference(std::vector<double> vectorA, std::vector<double> vectorB) {
+    std::transform(vectorA.begin(), vectorA.end(), vectorB.begin(), vectorB.begin(), std::minus<double>());
+    return vectorB;
+  }
+
+  static double ComputeArea(std::vector<double> vectorA, std::vector<double> vectorB) {
+    return sqrt(abs(ComputeScalarProduct(vectorA, vectorA) *
+        ComputeScalarProduct(vectorB, vectorB) - pow(ComputeScalarProduct(vectorA, vectorB), 2)));
+  }
+
+  static double ComputeScalarProduct(std::vector<double> vectorA, std::vector<double> vectorB) {
+    std::transform(vectorA.begin(), vectorA.end(), vectorB.begin(), vectorB.begin(), std::multiplies<double>());
+    return std::accumulate(vectorB.begin(), vectorB.end(), 0);
+  }
+};
+}
+
+#endif //SPLINELIB_PROJECTION_H
---
+++ test/projection_curve_test.cc
@@ -0,0 +1,63 @@
+/* Copyright 2018 Chair for Computational Analysis of Technical Systems, RWTH Aachen University
+
+This file is part of SplineLib.
+
+SplineLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3 of the License.
+
+SplineLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with SplineLib.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+#include "gmock/gmock.h"
+
+#include "b_spline.h"
+#include "projection.h"
+
+using testing::Test;
+using testing::DoubleEq;
+using testing::DoubleNear;
+
+class ABSpline2 : public Test {
+ public:
+  ABSpline2() {
+    knot_vector = {baf::KnotVector({0, 0, 0, 0, 0.2, 0.4, 0.6, 0.8, 1, 1, 1, 1})};
+    degree = {3};
+    control_points = {
+        baf::ControlPoint(std::vector<double>({100, 100})),
+        baf::ControlPoint(std::vector<double>({140, 196})),
+        baf::ControlPoint(std::vector<double>({200, 240})),
+        baf::ControlPoint(std::vector<double>({260, 164})),
+        baf::ControlPoint(std::vector<double>({340, 164})),
+        baf::ControlPoint(std::vector<double>({400, 240})),
+        baf::ControlPoint(std::vector<double>({460, 196})),
+        baf::ControlPoint(std::vector<double>({500, 100}))
+    };
+  }
+
+ protected:
+  std::array<baf::KnotVector, 1> knot_vector;
+  std::array<int, 1> degree;
+  std::vector<baf::ControlPoint> control_points;
+};
+
+TEST_F(ABSpline2, CloseToCenter) {
+  ASSERT_THAT(spl::Projection<1>::ProjectionOnSpline({332, 200},
+                                                     new spl::BSpline<1>(knot_vector, degree, control_points))[0],
+              DoubleNear(0.6223419238, 0.0001));
+}
+
+TEST_F(ABSpline2, RightOfLastKnot) {
+  ASSERT_THAT(spl::Projection<1>::ProjectionOnSpline({800, 200},
+                                                     new spl::BSpline<1>(knot_vector, degree, control_points))[0],
+              DoubleEq(1));
+}
+
+TEST_F(ABSpline2, LeftOfFirstKnot) {
+  ASSERT_THAT(spl::Projection<1>::ProjectionOnSpline({0, 0},
+                                                     new spl::BSpline<1>(knot_vector, degree, control_points))[0],
+              DoubleEq(0));
+}
