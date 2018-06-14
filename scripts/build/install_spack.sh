93a9f267ab7c51de4954766439fa171cfe51b84b
Markus Frings <frings@cats.rwt-aachen.de>
2018-06-14 16:49:09 +0200

    Develop travis improvements.

---
+++ scripts/build/install_spack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+if ! which spack >/dev/null; then
+    mkdir -p $SPACK_ROOT
+    git clone --depth 50 https://github.com/spack/spack.git $SPACK_ROOT
+    echo -e "config:""\n  build_jobs:"" 2" > $SPACK_ROOT/etc/spack/config.yaml;
+    spack bootstrap
+fi
