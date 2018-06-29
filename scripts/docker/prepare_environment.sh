c631a37038e198dafbe9174ab22ba2c3a9c1100b
Markus Frings <frings@cats.rwt-aachen.de>
2018-06-29 22:11:08 +0200

    Update Dockerfiles.

--- scripts/docker/prepare_environment.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-apt-get update
-apt-get -y upgrade
-apt-get install -y build-essential git python curl unzip python-pip
--- scripts/docker/prepare_googletest.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-. /code/bashrc
-spack load gcc
-spack compiler find
-spack install googletest+gmock
