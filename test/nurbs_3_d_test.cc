cc8fa55ae91533cd2cc3805fc02bab8695d3385b
cm314 <corinna.mueller@rwth-aachen.de>
2018-07-19 12:11:29 +0200

    adjust folder for cpplint.

--- test/nurbs_3_d_test.cc
+++ test/nurbs_3_d_test.cc
@@ -44,7 +44,8 @@ class A3DNurbsWithAllWeights1 : public Test {
         baf::ControlPoint(std::vector<double>({0.0, 2.0})),
         baf::ControlPoint(std::vector<double>({5.0, 2.0}))
     };
-    std::shared_ptr<std::array<baf::KnotVector, 3>> knot_vector_ptr = std::make_shared<std::array<baf::KnotVector, 3>>(knot_vector);
+    std::shared_ptr<std::array<baf::KnotVector, 3>>
+        knot_vector_ptr = std::make_shared<std::array<baf::KnotVector, 3>>(knot_vector);
     nurbs_ = std::make_unique<spl::NURBS<3>>(knot_vector_ptr, degree, control_points, weights);
     bspline_ = std::make_unique<spl::BSpline<3>>(knot_vector_ptr, degree, control_points);
   }
