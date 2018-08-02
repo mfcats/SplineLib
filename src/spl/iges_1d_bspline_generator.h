a782f9a054ad9d2892ce508b2d912a041be4ce2d
Christoph Susen <csx@web.de>
2018-08-02 13:32:36 +0200

    Simplify trim method to remove whitespace from strings.

--- src/spl/iges_1d_bspline_generator.h
+++ src/spl/iges_1d_bspline_generator.h
@@ -143,21 +143,14 @@ class IGES1DBSplineGenerator : public BSplineGenerator<1> {
     return number;
   }
 
-  inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
-  {
-    s.erase(0, s.find_first_not_of(t));
+  static inline std::string trim(std::string s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+      return !std::isspace(ch);
+    }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+      return !std::isspace(ch);
+    }).base(), s.end());
     return s;
-  }
-
-  inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
-  {
-    s.erase(s.find_last_not_of(t) + 1);
-    return s;
-  }
-
-  inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
-  {
-    return ltrim(rtrim(s, t), t);
   }
 
   std::string filename_;
