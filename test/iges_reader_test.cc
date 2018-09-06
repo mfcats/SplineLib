df253aa0a5acd03811f2cb2144eff2c1a719f92b
Christoph Susen <csx@web.de>
2018-09-06 10:09:22 +0200

    Additional tests for the iges spline generators.

--- test/iges_reader_test.cc
+++ test/iges_reader_test.cc
@@ -35,12 +35,24 @@ TEST_F(AnIGESReader, Read1DBSplineFromIGESFile) { // NOLINT
   spl::IGES1DBSplineGenerator reader = spl::IGES1DBSplineGenerator(path_to_iges_file);
   reader.ReadIGESFile(4);
   std::unique_ptr<spl::BSpline<1>> spline = std::make_unique<spl::BSpline<1>>(reader);
-  ASSERT_THAT(spline->Evaluate({ParamCoord{0.0}}, {0})[0], DoubleNear(-2.23308, 0.005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{0.0}}, {0})[0], DoubleNear(-2.23308, 0.0005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{0.0}}, {1})[0], DoubleNear(-0.01433, 0.0005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{0.0}}, {2})[0], DoubleNear(-0.51255, 0.0005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{1.0}}, {0})[0], DoubleNear(-1.3353, 0.0005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{1.0}}, {1})[0], DoubleNear(0.450443, 0.0005));
+  ASSERT_THAT(spline->Evaluate({ParamCoord{1.0}}, {2})[0], DoubleNear(-0.023586, 0.0005));
 }
 
 TEST_F(AnIGESReader, Read2DNURBSFromIGESFile) { // NOLINT
   spl::IGES2DNURBSGenerator reader2 = spl::IGES2DNURBSGenerator(path_to_iges_file);
   reader2.ReadIGESFile(2);
   std::unique_ptr<spl::NURBS<2>> spline2 = std::make_unique<spl::NURBS<2>>(reader2);
-  ASSERT_THAT(spline2->Evaluate({ParamCoord{0.0}, ParamCoord{0.0}}, {0})[0], DoubleNear(0, 0.005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{0.0}, ParamCoord{0.0}}, {0})[0], DoubleNear(0, 0.0005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{0.0}, ParamCoord{0.0}}, {1})[0], DoubleNear(0, 0.0005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{0.0}, ParamCoord{0.0}}, {2})[0], DoubleNear(-1, 0.0005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{1.0}, ParamCoord{1.0}}, {0})[0], DoubleNear(0, 0.0005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{1.0}, ParamCoord{1.0}}, {1})[0], DoubleNear(0, 0.0005));
+  ASSERT_THAT(spline2->Evaluate({ParamCoord{1.0}, ParamCoord{1.0}}, {2})[0], DoubleNear(0, 0.0005));
 }
+
+
