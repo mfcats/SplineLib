9e3d41633a74a2feb0a075e5672420b966cb9099
Christoph Susen <csx@web.de>
2018-10-29 16:16:16 +0100

    Remove linting errors.

--- test/iga/basis_function_handler_test.cc
+++ test/iga/basis_function_handler_test.cc
@@ -16,94 +16,15 @@ You should have received a copy of the GNU Lesser General Public License along w
 #include <vector>
 
 #include "gmock/gmock.h"
-#include "nurbs.h"
+
 #include "basis_function_handler.h"
+#include "iga_test_spline.h"
+
+#include "nurbs.h"
 
 using testing::DoubleNear;
-using testing::Test;
 
-class ABasisFunctionHandler : public Test {
- public:
-  std::array<baf::KnotVector, 2> knot_vector =
-      {baf::KnotVector({ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0.4}, ParamCoord{0.5},
-                        ParamCoord{0.6}, ParamCoord{1}, ParamCoord{1}, ParamCoord{1}, ParamCoord{1}}),
-       baf::KnotVector({ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0.5}, ParamCoord{0.5},
-                        ParamCoord{0.5}, ParamCoord{1}, ParamCoord{1}, ParamCoord{1}, ParamCoord{1}})};
-
-  std::array<Degree, 2> degree = {Degree{3}, Degree{3}};
-
-  std::vector<double> weights = {1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1,
-                                 1, 1, 1, 1, 1, 1, 1};
-
-  std::vector<baf::ControlPoint> control_points = {
-      baf::ControlPoint(std::vector<double>({0.0, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.16, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.32, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.48, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.64, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.8, -1.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({1.0, -1.0, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({0.0, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.16, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.32, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.48, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.64, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.8, -0.66, 0.0})),
-      baf::ControlPoint(std::vector<double>({1.0, -0.66, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({0.0, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.16, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.32, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.48, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.64, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.8, -0.33, 0.0})),
-      baf::ControlPoint(std::vector<double>({1.0, -0.33, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({0.0, 0.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.16, 0.16, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.32, 0.32, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.48, 0.48, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.64, 0.64, 0.0})),
-      baf::ControlPoint(std::vector<double>({0.8, 0.8, 0.0})),
-      baf::ControlPoint(std::vector<double>({1.0, 1.0, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({-0.33, 0.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 0.16, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 0.32, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 0.48, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 0.64, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 0.8, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.33, 1.0, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({-0.66, 0.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 0.16, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 0.32, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 0.48, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 0.64, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 0.8, 0.0})),
-      baf::ControlPoint(std::vector<double>({-0.66, 1.0, 0.0})),
-
-      baf::ControlPoint(std::vector<double>({-1.0, 0.0, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 0.16, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 0.32, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 0.48, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 0.64, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 0.8, 0.0})),
-      baf::ControlPoint(std::vector<double>({-1.0, 1.0, 0.0}))
-  };
-
-  std::array<std::shared_ptr<baf::KnotVector>, 2> kv_ptr = {std::make_shared<baf::KnotVector>(knot_vector[0]),
-                                                            std::make_shared<baf::KnotVector>(knot_vector[1])};
-  std::shared_ptr<spl::NURBS<2>> nurbs_ = std::make_shared<spl::NURBS<2>>(kv_ptr, degree, control_points, weights);
-};
-
-TEST_F(ABasisFunctionHandler, NURBSBasisFunctionEvaluation) { // NOLINT
+TEST_F(AnIGATestSpline, TestBasisFunctionHandler) { // NOLINT
   iga::BasisFunctionHandler basis_function_handler(nurbs_);
   std::vector<double> splinelib_nurbs_baf =
       basis_function_handler.EvaluateAllNonZeroNURBSBasisFunctions(std::array<ParamCoord, 2>({ParamCoord{0.55},
@@ -115,7 +36,7 @@ TEST_F(ABasisFunctionHandler, NURBSBasisFunctionEvaluation) { // NOLINT
   }
 }
 
-TEST_F(ABasisFunctionHandler, NURBSBasisFunctionDerivativeEvaluation) { // NOLINT
+TEST_F(AnIGATestSpline, TestBasisFunctionHandlerDerivative) { // NOLINT
   iga::BasisFunctionHandler basis_function_handler(nurbs_);
   std::array<std::vector<double>, 2> splinelib_nurbs_baf_der =
       basis_function_handler.EvaluateAllNonZeroNURBSBasisFunctionDerivatives(
@@ -127,8 +48,8 @@ TEST_F(ABasisFunctionHandler, NURBSBasisFunctionDerivativeEvaluation) { // NOLIN
         std::vector<double>({-0.044972, -0.140697, -0.097287, -0.011683, -0.270971, -0.847739, -0.586179, -0.070393,
                              -0.238953, -0.747572, -0.516917, -0.062076, 0.554896, 1.736009, 1.200382, 0.144152})});
   for (int i = 0; i < 2; ++i) {
-    for (int j = 0; j < splinelib_nurbs_baf_der[i].size(); ++j) {
-    ASSERT_THAT(splinelib_nurbs_baf_der[i][j], DoubleNear(matlab_nurbs_baf_der[i][j], 0.00005));
+    for (int j = 0; j < splinelib_nurbs_baf_der.at(i).size(); ++j) {
+    ASSERT_THAT(splinelib_nurbs_baf_der.at(i).at(j), DoubleNear(matlab_nurbs_baf_der.at(i).at(j), 0.00005));
     }
   }
 }
