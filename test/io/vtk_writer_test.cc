0a059cfe1ef18406ad778eca86975b6a7a53eb03
Christoph Susen <csx@web.de>
2018-11-01 12:21:14 +0100

    Add test to improve code coverage.

--- test/io/vtk_writer_test.cc
+++ test/io/vtk_writer_test.cc
@@ -146,3 +146,50 @@ TEST_F(A3DBNURBSVTKWriter, CreatesVTKFile) {  // NOLINT
   ASSERT_THAT(file.find("CELL_TYPES 24\n"), Ne(std::string::npos));
   remove("3d_spline.vtk");
 }
+
+class A4DBNURBSVTKWriter : public Test {  // NOLINT
+ public:
+  A4DBNURBSVTKWriter() : vtk_writer_(std::make_unique<io::VTKWriter>()) {
+    std::array<Degree, 4> degree = {Degree{1}, Degree{1}, Degree{1}, Degree{1}};
+    std::array<std::shared_ptr<baf::KnotVector>, 4> knot_vector = {
+        std::make_shared<baf::KnotVector>(baf::KnotVector(
+            {ParamCoord{0}, ParamCoord{0}, ParamCoord{1}, ParamCoord{1}})),
+        std::make_shared<baf::KnotVector>(baf::KnotVector(
+            {ParamCoord{0}, ParamCoord{0}, ParamCoord{1}, ParamCoord{1}})),
+        std::make_shared<baf::KnotVector>(baf::KnotVector(
+            {ParamCoord{0}, ParamCoord{0}, ParamCoord{1}, ParamCoord{1}})),
+        std::make_shared<baf::KnotVector>(baf::KnotVector(
+            {ParamCoord{0}, ParamCoord{0}, ParamCoord{1}, ParamCoord{1}}))};
+    std::vector<baf::ControlPoint> control_points = {
+        baf::ControlPoint(std::vector<double>({0.0, 0.0, 0.0})),
+        baf::ControlPoint(std::vector<double>({2.0, 0.3, -0.1})),
+        baf::ControlPoint(std::vector<double>({0.2, 2.2, 0.2})),
+        baf::ControlPoint(std::vector<double>({1.5, 2.0, 0.0})),
+        baf::ControlPoint(std::vector<double>({0.3, -0.1, 1.2})),
+        baf::ControlPoint(std::vector<double>({1.5, 0.1, 1.8})),
+        baf::ControlPoint(std::vector<double>({0.1, 2.2, 1.6})),
+        baf::ControlPoint(std::vector<double>({1.7, 2.0, 2.2})),
+        baf::ControlPoint(std::vector<double>({0.0, 0.0, 0.0})),
+        baf::ControlPoint(std::vector<double>({2.0, 0.3, -0.1})),
+        baf::ControlPoint(std::vector<double>({0.2, 2.2, 0.2})),
+        baf::ControlPoint(std::vector<double>({1.5, 2.0, 0.0})),
+        baf::ControlPoint(std::vector<double>({0.3, -0.1, 1.2})),
+        baf::ControlPoint(std::vector<double>({1.5, 0.1, 1.8})),
+        baf::ControlPoint(std::vector<double>({0.1, 2.2, 1.6})),
+        baf::ControlPoint(std::vector<double>({1.7, 2.0, 2.2}))
+    };
+    std::vector<double> weights = {3.0, 0.5, 1.8, 2.0, 1.8, 1.0, 0.8, 0.2, 3.0, 0.5, 1.8, 2.0, 1.8, 1.0, 0.8, 0.2};
+    nurbs_4d_ = std::make_shared<spl::NURBS<4>>(knot_vector, degree, control_points, weights);
+  }
+
+ protected:
+  std::shared_ptr<spl::NURBS<4>> nurbs_4d_;
+  std::unique_ptr<io::VTKWriter> vtk_writer_;
+};
+
+TEST_F(A4DBNURBSVTKWriter, AddSplineThrowsFor4DNURBS) {  // NOLINT
+  std::any nurbs_4d_any = std::make_any<std::shared_ptr<spl::NURBS<4>>>(nurbs_4d_);
+  ASSERT_THROW(vtk_writer_->WriteFile({nurbs_4d_any}, "4d_spline.vtk", {{4, 3, 2, 1}}), std::runtime_error);
+  remove("4d_spline.vtk");
+}
+
