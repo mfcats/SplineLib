f24a45ad6f67a6bfa4eff5639d05b1c9e50e89ea
christophsusen <christoph.susen@rwth-aachen.de>
2018-11-12 15:59:04 +0100

    Solution can be written to a vtk file as point data.

--- src/iga/solution_vtk_writer.h
+++ src/iga/solution_vtk_writer.h
@@ -17,6 +17,7 @@ You should have received a copy of the GNU Lesser General Public License along w
 
 #include <armadillo>
 #include <fstream>
+#include <string>
 #include <vector>
 
 #include "nurbs.h"
