14afc94e0c0d1536168057bbde47980f70ad47d6
cm314 <corinna.mueller@rwth-aachen.de>
2018-12-03 13:14:46 +0100

    add knot removal test.

---
+++ test/spl/knot_removal_test.cc
@@ -0,0 +1,81 @@
+/* Copyright 2018 Chair for Computational Analysis of Technical Systems, RWTH Aachen University
+
+This file is part of SplineLib.
+
+SplineLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3 of the License.
+
+SplineLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with SplineLib.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+#include "gmock/gmock.h"
+
+#include "b_spline.h"
+#include "nurbs.h"
+#include "random_b_spline_generator.h"
+#include "random_nurbs_generator.h"
+
+using testing::Test;
+using testing::DoubleEq;
+using testing::DoubleNear;
+
+class BSplineFig5_27 : public Test {  // NOLINT
+ public:
+  BSplineFig5_27() {
+    std::array<Degree, 1> degree = {Degree{3}};
+    KnotVectors<1> knot_vector_before = {std::make_shared<baf::KnotVector>(
+        baf::KnotVector({ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0}, ParamCoord{0.3}, ParamCoord{0.5},
+                         ParamCoord{0.5}, ParamCoord{0.7}, ParamCoord{0.7}, ParamCoord{1}, ParamCoord{1}, ParamCoord{1},
+                         ParamCoord{1}}))};
+    std::vector<baf::ControlPoint> control_points = {
+        baf::ControlPoint(std::vector<double>({0.1, 0.0})),
+        baf::ControlPoint(std::vector<double>({0.0, 1.0})),
+        baf::ControlPoint(std::vector<double>({1.0, 2.0})),
+        baf::ControlPoint(std::vector<double>({2.5, 2.25})),
+        baf::ControlPoint(std::vector<double>({3.0, 2.15})),
+        baf::ControlPoint(std::vector<double>({4.0, 1.8})),
+        baf::ControlPoint(std::vector<double>({4.5, 1.0})),
+        baf::ControlPoint(std::vector<double>({4.3, 0.0})),
+        baf::ControlPoint(std::vector<double>({6.0, 0.0}))
+    };
+    bspline_1d_before_ = std::make_shared<spl::BSpline<1>>(knot_vector_before, degree, control_points);
+    spl::BSpline<1> b_spline_after(*bspline_1d_before_);
+    bspline_1d_after_ = std::make_shared<spl::BSpline<1>>(b_spline_after);
+  }
+
+ protected:
+  std::shared_ptr<spl::BSpline<1>> bspline_1d_before_;
+  std::shared_ptr<spl::BSpline<1>> bspline_1d_after_;
+};
+
+TEST_F(BSplineFig5_27, InsertsKnot2_5Correctly) {  // NOLINT
+  bspline_1d_after_->RemoveKnot(ParamCoord(0.3), 0);
+  ASSERT_THAT(bspline_1d_after_->GetKnotVector(0)->GetNumberOfKnots(),
+              bspline_1d_before_->GetKnotVector(0)->GetNumberOfKnots() - 1);
+  ASSERT_THAT(bspline_1d_after_->GetKnotVector(0)->GetKnot(8).get(), DoubleEq(1));
+  ASSERT_THAT(bspline_1d_after_->GetNumberOfControlPoints(), bspline_1d_before_->GetNumberOfControlPoints() - 1);
+  std::vector<baf::ControlPoint> new_control_points = {
+      baf::ControlPoint(std::vector<double>({0.1, 0.0})),
+      baf::ControlPoint(std::vector<double>({-0.1, 1.5})),
+      baf::ControlPoint(std::vector<double>({1.5, 2.5})),
+      baf::ControlPoint(std::vector<double>({3.0, 2.15})),
+      baf::ControlPoint(std::vector<double>({4.0, 1.8})),
+      baf::ControlPoint(std::vector<double>({4.5, 1.0})),
+      baf::ControlPoint(std::vector<double>({4.3, 0.0})),
+      baf::ControlPoint(std::vector<double>({6.0, 0.0}))
+  };
+  for (int i = 0; i < static_cast<int>(new_control_points.size()); ++i) {
+    for (int j = 0; j < 2; ++j) {
+      ASSERT_THAT(bspline_1d_after_->GetControlPoint({i}, j), DoubleEq(new_control_points[i].GetValue(j)));
+    }
+  }
+  for (int i = 0; i <= 50; ++i) {
+    std::array<ParamCoord, 1> param_coord{ParamCoord(i / 10.0)};
+    ASSERT_THAT(bspline_1d_after_->Evaluate(param_coord, {0})[0],
+                DoubleEq(bspline_1d_before_->Evaluate(param_coord, {0})[0]));
+  }
+}
