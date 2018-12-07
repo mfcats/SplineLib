18d9bdb39e05f8bd13cda32770e0a434ad505830
christophsusen <christoph.susen@rwth-aachen.de>
2018-12-07 18:33:05 +0100

    Simplify functions in element generator.

--- src/iga/elm/element_generator.h
+++ src/iga/elm/element_generator.h
@@ -27,12 +27,11 @@ namespace elm {
 template<int DIM>
 class ElementGenerator {
  public:
-  explicit ElementGenerator(std::shared_ptr<spl::Spline<DIM>> spl) : spline_(std::move(spl)) {}
+  explicit ElementGenerator(std::shared_ptr<spl::Spline<DIM>> spl) : spl_(std::move(spl)) {}
 
   std::vector<iga::elm::Element> GetElementList(int dir) const {
     std::vector<iga::elm::Element> elements;
-    for (uint64_t k = 0; k < spline_->GetKnotVector(dir)->GetNumberOfKnots() - spline_->GetDegree(dir).get() - 1;
-         ++k) {
+    for (uint64_t k = 0; k < spl_->GetKnotVector(dir)->GetNumberOfKnots() - spl_->GetDegree(dir).get() - 1; ++k) {
       if ((GetLowerElementBound(k, dir).get() - GetHigherElementBound(k, dir).get()) != 0) {
         elements.emplace_back(Element(1, GetElementNodes(k, dir)));
       }
@@ -40,7 +39,7 @@ class ElementGenerator {
     return elements;
   }
 
-  std::array<int, DIM> GetNumberOfElements() {
+  std::array<int, DIM> GetNumberOfElements() const {
     std::array<int, DIM> num_elms{};
     for (int i = 0; i < DIM; ++i) {
       num_elms[i] = GetElementList(i).size();
@@ -64,46 +63,36 @@ class ElementGenerator {
   }
 
   int GetElementNumberAtParamCoord(std::array<ParamCoord, 2> param_coord) const {
-    uint64_t element_number_xi = 0;
-    uint64_t element_number_eta = 0;
+    int element_number_xi = 0;
+    int element_number_eta = 0;
     std::vector<ParamCoord> unique_knots_xi = GetUniqueKnots(0);
     std::vector<ParamCoord> unique_knots_eta = GetUniqueKnots(1);
     for (uint64_t i = 0; i < unique_knots_xi.size() - 1; ++i) {
       if ((unique_knots_xi[i].get() <= param_coord[0].get()) &&
           (unique_knots_xi[i + 1].get() > param_coord[0].get())) {
-        element_number_xi = i;
+        element_number_xi = static_cast<int>(i);
       }
     }
     for (uint64_t i = 0; i < unique_knots_eta.size() - 1; ++i) {
       if ((unique_knots_eta[i].get() <= param_coord[1].get()) &&
           (unique_knots_eta[i + 1].get() > param_coord[1].get())) {
-        element_number_eta = i;
+        element_number_eta = static_cast<int>(i);
       }
     }
-    return Get1DElementIndex(element_number_xi, element_number_eta);
+    return Get1DElementIndex({element_number_xi, element_number_eta});
   }
 
-  std::array<int, 2> Get2DElementIndices(int element_number) const {
-    element_number += 1;
-    int number_of_elements_xi = static_cast<int>(GetElementList(0).size());
-    int q = element_number / number_of_elements_xi;
-    int r = element_number % number_of_elements_xi;
-    std::array<int, 2> element_indices_2d{};
-    if (r == 0) {
-      element_indices_2d[1] = q - 1;
-      element_indices_2d[0] = number_of_elements_xi - 1;
-    } else {
-      element_indices_2d[1] = q;
-      element_indices_2d[0] = r - 1;
-    }
-    return element_indices_2d;
+  std::array<int, DIM> Get2DElementIndices(int elm_num) const {
+    util::MultiIndexHandler<DIM> mult_ind_handl_elm(GetNumberOfElements());
+    mult_ind_handl_elm = mult_ind_handl_elm + elm_num;
+    return mult_ind_handl_elm.GetIndices();
   }
 
  private:
   std::vector<ParamCoord> GetInternalKnots(int dir) const {
-    std::vector<ParamCoord> knots = spline_->GetKnots()[dir];
+    std::vector<ParamCoord> knots = spl_->GetKnots()[dir];
     std::vector<ParamCoord> internal_knots;
-    int degree = spline_->GetDegree(dir).get();
+    int degree = spl_->GetDegree(dir).get();
     for (auto j = uint64_t(degree); j < knots.size() - degree; ++j) {
       internal_knots.emplace_back(knots[j]);
     }
@@ -122,26 +111,25 @@ class ElementGenerator {
     return unique_knots;
   }
 
-  int Get1DElementIndex(uint64_t element_number_xi, uint64_t element_number_eta) const {
-    util::MultiIndexHandler<2> multi_index_handler({static_cast<int>(GetElementList(0).size()),
-                                                    static_cast<int>(GetElementList(1).size())});
-    multi_index_handler.SetIndices({static_cast<int>(element_number_xi), static_cast<int>(element_number_eta)});
-    return multi_index_handler.Get1DIndex();
+  int Get1DElementIndex(std::array<int, DIM> element_indices) const {
+    util::MultiIndexHandler<DIM> mult_ind_handl_elm(GetNumberOfElements());
+    mult_ind_handl_elm.SetIndices(element_indices);
+    return mult_ind_handl_elm.Get1DIndex();
   }
 
   ParamCoord GetLowerElementBound(uint64_t currentKnot, int dir) const {
-    return spline_->GetKnotVector(dir)->GetKnots()[currentKnot];
+    return spl_->GetKnotVector(dir)->GetKnots()[currentKnot];
   }
 
   ParamCoord GetHigherElementBound(uint64_t currentKnot, int dir) const {
-    return spline_->GetKnotVector(dir)->GetKnots()[currentKnot + 1];
+    return spl_->GetKnotVector(dir)->GetKnots()[currentKnot + 1];
   }
 
   std::vector<ParamCoord> GetElementNodes(uint64_t currentKnot, int dir) const {
     return {GetLowerElementBound(currentKnot, dir), GetHigherElementBound(currentKnot, dir)};
   }
 
-  std::shared_ptr<spl::Spline<DIM>> spline_;
+  std::shared_ptr<spl::Spline<DIM>> spl_;
 };
 }  // namespace elm
 }  // namespace iga
