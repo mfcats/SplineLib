2eb8fe7e710523189cdbb8364a8b8fa9f17111ac
cm314 <corinna.mueller@rwth-aachen.de>
2019-03-13 10:51:14 +0100

    improve random knot insertion and removal tests.

--- test/spl/knot_insertion_and_removal_test.cc
+++ test/spl/knot_insertion_and_removal_test.cc
@@ -25,7 +25,7 @@ using testing::DoubleNear;
 class Random1DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random1DBSplineForKnotInsertionAndRemoval() :
-      param_coord_(ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}) {
+      param_coord_(ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}) {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomBSplineGenerator<1> spline_generator(limits, 10, 3);
     spl::BSpline<1> b_spline(spline_generator);
@@ -36,12 +36,13 @@ class Random1DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::BSpline<1>>(insertion_spline);
 
     spl::BSpline<1> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_, 0, 1e-15);
+    removed_ = removal_spline.RemoveKnot(param_coord_, 0, 1e-10);
     after_removal_ = std::make_shared<spl::BSpline<1>>(removal_spline);
   }
 
  protected:
   ParamCoord param_coord_;
+  size_t removed_;
   std::shared_ptr<spl::BSpline<1>> original_;
   std::shared_ptr<spl::BSpline<1>> after_insertion_;
   std::shared_ptr<spl::BSpline<1>> after_removal_;
@@ -66,6 +67,10 @@ TEST_F(Random1DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
   }
 }
 
+TEST_F(Random1DBSplineForKnotInsertionAndRemoval, HasRemovedKnot) {  // NOLINT
+  ASSERT_THAT(removed_, 1);
+}
+
 TEST_F(Random1DBSplineForKnotInsertionAndRemoval, HasOneLessKnotAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 1);
@@ -79,7 +84,7 @@ TEST_F(Random1DBSplineForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKn
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
   }
 }
@@ -97,7 +102,7 @@ TEST_F(Random1DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
 class Random1DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random1DNURBSForKnotInsertionAndRemoval() :
-      param_coord_(ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}) {
+      param_coord_(ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}) {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomNURBSGenerator<1> spline_generator(limits, 10, 3);
     spl::NURBS<1> nurbs(spline_generator);
@@ -108,12 +113,13 @@ class Random1DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::NURBS<1>>(insertion_spline);
 
     spl::NURBS<1> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_, 0, 1e-15);
+    removed_ = removal_spline.RemoveKnot(param_coord_, 0, 1e-10);
     after_removal_ = std::make_shared<spl::NURBS<1>>(removal_spline);
   }
 
  protected:
   ParamCoord param_coord_;
+  size_t removed_;
   std::shared_ptr<spl::NURBS<1>> original_;
   std::shared_ptr<spl::NURBS<1>> after_insertion_;
   std::shared_ptr<spl::NURBS<1>> after_removal_;
@@ -138,6 +144,10 @@ TEST_F(Random1DNURBSForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfterK
   }
 }
 
+TEST_F(Random1DNURBSForKnotInsertionAndRemoval, HasRemovedKnot) {  // NOLINT
+  ASSERT_THAT(removed_, 1);
+}
+
 TEST_F(Random1DNURBSForKnotInsertionAndRemoval, HasOneLessKnotAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 1);
@@ -151,9 +161,9 @@ TEST_F(Random1DNURBSForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKnot
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
-    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-12));
+    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-10));
   }
 }
 
@@ -170,9 +180,9 @@ TEST_F(Random1DNURBSForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfterK
 class Random2DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random2DBSplineForKnotInsertionAndRemoval() : param_coord_{
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}} {
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}} {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomBSplineGenerator<2> spline_generator(limits, 10, 3);
     spl::BSpline<2> b_spline(spline_generator);
@@ -185,14 +195,15 @@ class Random2DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::BSpline<2>>(insertion_spline);
 
     spl::BSpline<2> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_[0], 0, 1e-12);
-    removal_spline.RemoveKnot(param_coord_[1], 1, 1e-12);
-    removal_spline.RemoveKnot(param_coord_[2], 1, 1e-12);
+    removed_[0] = removal_spline.RemoveKnot(param_coord_[0], 0, 1e-10);
+    removed_[1] = removal_spline.RemoveKnot(param_coord_[1], 1, 1e-10);
+    removed_[2] = removal_spline.RemoveKnot(param_coord_[2], 1, 1e-10);
     after_removal_ = std::make_shared<spl::BSpline<2>>(removal_spline);
   }
 
  protected:
   std::array<ParamCoord, 3> param_coord_;
+  std::array<size_t, 3> removed_;
   std::shared_ptr<spl::BSpline<2>> original_;
   std::shared_ptr<spl::BSpline<2>> after_insertion_;
   std::shared_ptr<spl::BSpline<2>> after_removal_;
@@ -221,6 +232,12 @@ TEST_F(Random2DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
   }
 }
 
+TEST_F(Random2DBSplineForKnotInsertionAndRemoval, HasRemovedKnots) {  // NOLINT
+  ASSERT_THAT(removed_[0], 1);
+  ASSERT_THAT(removed_[1], 1);
+  ASSERT_THAT(removed_[2], 1);
+}
+
 TEST_F(Random2DBSplineForKnotInsertionAndRemoval, HasOneLessKnotPerDirectionAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 1);
@@ -237,7 +254,7 @@ TEST_F(Random2DBSplineForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKn
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
   }
 }
@@ -256,9 +273,9 @@ TEST_F(Random2DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
 class Random2DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random2DNURBSForKnotInsertionAndRemoval() : param_coord_{
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}} {
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}} {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomNURBSGenerator<2> spline_generator(limits, 8, 3);
     spl::NURBS<2> nurbs(spline_generator);
@@ -271,14 +288,15 @@ class Random2DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::NURBS<2>>(insertion_spline);
 
     spl::NURBS<2> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_[0], 0, 1e-12);
-    removal_spline.RemoveKnot(param_coord_[1], 0, 1e-12);
-    removal_spline.RemoveKnot(param_coord_[2], 1, 1e-12);
+    removed_[0] = removal_spline.RemoveKnot(param_coord_[0], 0, 1e-10);
+    removed_[1] = removal_spline.RemoveKnot(param_coord_[1], 0, 1e-10);
+    removed_[2] = removal_spline.RemoveKnot(param_coord_[2], 1, 1e-10);
     after_removal_ = std::make_shared<spl::NURBS<2>>(removal_spline);
   }
 
  protected:
   std::array<ParamCoord, 3> param_coord_;
+  std::array<size_t, 3> removed_;
   std::shared_ptr<spl::NURBS<2>> original_;
   std::shared_ptr<spl::NURBS<2>> after_insertion_;
   std::shared_ptr<spl::NURBS<2>> after_removal_;
@@ -307,6 +325,12 @@ TEST_F(Random2DNURBSForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfterK
   }
 }
 
+TEST_F(Random2DNURBSForKnotInsertionAndRemoval, HasRemovedKnots) {  // NOLINT
+  ASSERT_THAT(removed_[0], 1);
+  ASSERT_THAT(removed_[1], 1);
+  ASSERT_THAT(removed_[2], 1);
+}
+
 TEST_F(Random2DNURBSForKnotInsertionAndRemoval, HasOneLessKnotPerDirectionAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 2);
@@ -323,9 +347,9 @@ TEST_F(Random2DNURBSForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKnot
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
-    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-12));
+    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-10));
   }
 }
 
@@ -343,9 +367,9 @@ TEST_F(Random2DNURBSForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfterK
 class Random3DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random3DBSplineForKnotInsertionAndRemoval() : param_coord_{
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}} {
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}} {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomBSplineGenerator<3> spline_generator(limits, 10, 3);
     spl::BSpline<3> b_spline(spline_generator);
@@ -358,14 +382,15 @@ class Random3DBSplineForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::BSpline<3>>(insertion_spline);
 
     spl::BSpline<3> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_[0], 0, 1e-15);
-    removal_spline.RemoveKnot(param_coord_[1], 1, 1e-15);
-    removal_spline.RemoveKnot(param_coord_[2], 2, 1e-15);
+    removed_[0] = removal_spline.RemoveKnot(param_coord_[0], 0, 1e-10);
+    removed_[1] = removal_spline.RemoveKnot(param_coord_[1], 1, 1e-10);
+    removed_[2] = removal_spline.RemoveKnot(param_coord_[2], 2, 1e-10);
     after_removal_ = std::make_shared<spl::BSpline<3>>(removal_spline);
   }
 
  protected:
   std::array<ParamCoord, 3> param_coord_;
+  std::array<size_t, 3> removed_;
   std::shared_ptr<spl::BSpline<3>> original_;
   std::shared_ptr<spl::BSpline<3>> after_insertion_;
   std::shared_ptr<spl::BSpline<3>> after_removal_;
@@ -398,6 +423,12 @@ TEST_F(Random3DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
   }
 }
 
+TEST_F(Random3DBSplineForKnotInsertionAndRemoval, HasRemovedKnots) {  // NOLINT
+  ASSERT_THAT(removed_[0], 1);
+  ASSERT_THAT(removed_[1], 1);
+  ASSERT_THAT(removed_[2], 1);
+}
+
 TEST_F(Random3DBSplineForKnotInsertionAndRemoval, HasOneLessKnotPerDirectionAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 1);
@@ -417,7 +448,7 @@ TEST_F(Random3DBSplineForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKn
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
   }
 }
@@ -437,9 +468,9 @@ TEST_F(Random3DBSplineForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfte
 class Random3DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
  public:
   Random3DNURBSForKnotInsertionAndRemoval() : param_coord_{
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)},
-      ParamCoord{util::Random::GetUniformRandom<double>(0.0, 1.0)}} {
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)},
+      ParamCoord{util::Random::GetUniformRandom<double>(0.01, 0.99)}} {
     std::array<ParamCoord, 2> limits = {ParamCoord{0.0}, ParamCoord{1.0}};
     spl::RandomNURBSGenerator<3> spline_generator(limits, 5, 3);
     spl::NURBS<3> nurbs(spline_generator);
@@ -452,14 +483,15 @@ class Random3DNURBSForKnotInsertionAndRemoval : public Test {  // NOLINT
     after_insertion_ = std::make_shared<spl::NURBS<3>>(insertion_spline);
 
     spl::NURBS<3> removal_spline(insertion_spline);
-    removal_spline.RemoveKnot(param_coord_[0], 0, 1e-15);
-    removal_spline.RemoveKnot(param_coord_[1], 1, 1e-15);
-    removal_spline.RemoveKnot(param_coord_[2], 2, 1e-15);
+    removed_[0] = removal_spline.RemoveKnot(param_coord_[0], 0, 1e-10);
+    removed_[1] = removal_spline.RemoveKnot(param_coord_[1], 1, 1e-10);
+    removed_[2] = removal_spline.RemoveKnot(param_coord_[2], 2, 1e-10);
     after_removal_ = std::make_shared<spl::NURBS<3>>(removal_spline);
   }
 
  protected:
   std::array<ParamCoord, 3> param_coord_;
+  std::array<size_t, 3> removed_;
   std::shared_ptr<spl::NURBS<3>> original_;
   std::shared_ptr<spl::NURBS<3>> after_insertion_;
   std::shared_ptr<spl::NURBS<3>> after_removal_;
@@ -492,6 +524,12 @@ TEST_F(Random3DNURBSForKnotInsertionAndRemoval, DoesNotChangeGeometricallyAfterK
   }
 }
 
+TEST_F(Random3DNURBSForKnotInsertionAndRemoval, HasRemovedKnots) {  // NOLINT
+  ASSERT_THAT(removed_[0], 1);
+  ASSERT_THAT(removed_[1], 1);
+  ASSERT_THAT(removed_[2], 1);
+}
+
 TEST_F(Random3DNURBSForKnotInsertionAndRemoval, HasOneLessKnotPerDirectionAfterKnotRemoval) {  // NOLINT
   ASSERT_THAT(after_removal_->GetKnotVector(0)->GetNumberOfKnots(),
               after_insertion_->GetKnotVector(0)->GetNumberOfKnots() - 1);
@@ -511,9 +549,9 @@ TEST_F(Random3DNURBSForKnotInsertionAndRemoval, DoesNotChangePhysicallyAfterKnot
   for (int point = 0; point < original_->GetNumberOfControlPoints(); ++point) {
     for (int dimension = 0; dimension < original_->GetDimension(); ++dimension) {
       ASSERT_THAT(after_removal_->GetControlPoint({point}, dimension),
-                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-12));
+                  DoubleNear(original_->GetControlPoint({point}, dimension), 1e-10));
     }
-    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-12));
+    ASSERT_THAT(after_removal_->GetWeight({point}), DoubleNear(original_->GetWeight({point}), 1e-10));
   }
 }
 
