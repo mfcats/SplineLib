618118426d7e900d9f917cb5b9aeef4929d8d148
christophsusen <christoph.susen@rwth-aachen.de>
2019-10-02 12:04:21 +0200

    Rename template parameter DIM to PARAMETRIC_DIMENSIONALITY.

--- src/baf/knot_vector.h
+++ src/baf/knot_vector.h
@@ -17,6 +17,7 @@ You should have received a copy of the GNU Lesser General Public License along w
 
 #include <array>
 #include <initializer_list>
+#include <memory>
 #include <utility>
 #include <vector>
 
