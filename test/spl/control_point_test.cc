bda5069a64e76e21e097651b1be35fc702bf53b2
christophsusen <christoph.susen@rwth-aachen.de>
2019-11-25 13:35:36 +0100

    Refactor control point tests.

--- test/spl/control_point_test.cc
+++ test/spl/control_point_test.cc
@@ -25,44 +25,96 @@ using namespace splinelib::src;
 
 class AControlPoint : public Test {
  public:
-  AControlPoint() : control_point({1.0, 2.0}), control_point_b({2.0, -1.0}), control_point_c({1.0, 0.7, 1.9}),
-      transMatrix({std::array<double, 4>({0.5, 0.0, 0.866, 0.0}),
-                   std::array<double, 4>({0.0, 1.0, 0.0, 0.0}),
-                   std::array<double, 4>({-0.866, 0.0, 0.5, 0.0}),
-                   std::array<double, 4>({0.0, 0.0, 0.0, 1.0})}),
-      scaling({1.0, 1.0, 1.0}) {}
+  AControlPoint() : control_point_a_({1.0, 2.0}), control_point_b_({2.0, -1.0}), control_point_c_({1.0, 0.7, 1.9}),
+                    transformation_matrix_({std::array<double, 4>({0.5, 0.0, 0.866, 0.0}),
+                                            std::array<double, 4>({0.0, 1.0, 0.0, 0.0}),
+                                            std::array<double, 4>({-0.866, 0.0, 0.5, 0.0}),
+                                            std::array<double, 4>({0.0, 0.0, 0.0, 1.0})}),
+                    scaling_({1.0, 1.0, 1.0}) {}
 
  protected:
-  baf::ControlPoint control_point;
-  baf::ControlPoint control_point_b;
-  baf::ControlPoint control_point_c;
-  std::array<std::array<double, 4>, 4> transMatrix;
-  std::array<double, 3> scaling;
+  baf::ControlPoint control_point_a_;
+  baf::ControlPoint control_point_b_;
+  baf::ControlPoint control_point_c_;
+  std::array<std::array<double, 4>, 4> transformation_matrix_;
+  std::array<double, 3> scaling_;
 };
 
+bool operator== (baf::ControlPoint const &lhs, baf::ControlPoint const &rhs) {
+  for (Dimension i{0}; i < Dimension{rhs.GetDimensionality()}; ++i) {
+    if (rhs[i] != lhs[i]) return false;
+  }
+  return true;
+}
+
+TEST_F(AControlPoint, CopyConstructorWorks) {  // NOLINT
+  baf::ControlPoint copy_of_a(control_point_a_);
+  ASSERT_TRUE(operator==(copy_of_a, control_point_a_));
+}
+
+TEST_F(AControlPoint, MoveConstructorWorks) {  // NOLINT
+  baf::ControlPoint copy_of_a(std::move(control_point_a_));
+  ASSERT_TRUE(operator==(copy_of_a, control_point_a_));
+}
+
+TEST_F(AControlPoint, CopyAssignmentOperatorWorks) {  // NOLINT
+  baf::ControlPoint copy_of_a = control_point_a_;
+  ASSERT_TRUE(operator==(copy_of_a, control_point_a_));
+}
+
+TEST_F(AControlPoint, MoveAssignmentOperatorWorks) {  // NOLINT
+  baf::ControlPoint copy_of_a = std::move(control_point_a_);
+  ASSERT_TRUE(operator==(copy_of_a, control_point_a_));
+}
+
 TEST_F(AControlPoint, ReturnsCorrectDimension) { // NOLINT
-  ASSERT_THAT(control_point.GetDimensionality(), 2);
+  ASSERT_THAT(control_point_a_.GetDimensionality(), 2);
 }
 
-TEST_F(AControlPoint, Returns1For0Dimension) { // NOLINT
-  ASSERT_THAT(control_point.GetValueForDimension(Dimension{0}), DoubleEq(1.0));
+TEST_F(AControlPoint, Returns1ForDimension0) { // NOLINT
+  ASSERT_THAT(control_point_a_.GetValueForDimension(Dimension{0}), DoubleEq(1.0));
+  ASSERT_THAT(control_point_a_[Dimension{0}], DoubleEq(1.0));
 }
 
-TEST_F(AControlPoint, Returns2For1Dimension) { // NOLINT
-  ASSERT_THAT(control_point.GetValueForDimension(Dimension{1}), DoubleEq(2.0));
+TEST_F(AControlPoint, Returns2ForDimension1) { // NOLINT
+  ASSERT_THAT(control_point_a_.GetValueForDimension(Dimension{1}), DoubleEq(2.0));
+  ASSERT_THAT(control_point_a_[Dimension{1}], DoubleEq(2.0));
 }
 
-TEST_F(AControlPoint, Returns3ForSum0Dimension) { // NOLINT
-  ASSERT_THAT((control_point + control_point_b).GetValueForDimension(Dimension{0}), DoubleEq(3.0));
+TEST_F(AControlPoint, Returns2_5ForDimension1AfterSetValue) { // NOLINT
+  baf::ControlPoint control_point(control_point_a_);
+  control_point_a_.SetValue(1, 2.5);
+  ASSERT_THAT(control_point_a_[Dimension{1}], DoubleEq(2.5));
+}
+
+TEST_F(AControlPoint, ReturnsCorrectResultAfterAdditionOfControlPoints) { // NOLINT
+  ASSERT_THAT((control_point_a_ + control_point_b_)[Dimension{0}], DoubleEq(3.0));
+  ASSERT_THAT((control_point_a_ + control_point_b_)[Dimension{1}], DoubleEq(1.0));
+}
+
+TEST_F(AControlPoint, ReturnsCorrectResultAfterSubtractionOfControlPoints) { // NOLINT
+  ASSERT_THAT((control_point_a_ - control_point_b_)[Dimension{0}], DoubleEq(-1.0));
+  ASSERT_THAT((control_point_a_ - control_point_b_)[Dimension{1}], DoubleEq(3.0));
+}
+
+TEST_F(AControlPoint, ReturnsCorrectResultAfterMultiplicationWithScalar) { // NOLINT
+  ASSERT_THAT((control_point_a_ * 0.5)[Dimension{0}], DoubleEq(0.5));
+  ASSERT_THAT((control_point_a_ * 0.5)[Dimension{1}], DoubleEq(1.0));
+  ASSERT_THAT((0.5 * control_point_a_)[Dimension{0}], DoubleEq(0.5));
+  ASSERT_THAT((0.5 * control_point_a_)[Dimension{1}], DoubleEq(1.0));
 }
 
 TEST_F(AControlPoint, PerformsTransformationCorrectly) { // NOLINT
-  baf::ControlPoint control_point_t = control_point_c.Transform(transMatrix, scaling);
-  ASSERT_THAT(control_point_t.GetValueForDimension(Dimension{0}), DoubleEq(2.1454));
+  baf::ControlPoint control_point_t = control_point_c_.Transform(transformation_matrix_, scaling_);
+  ASSERT_THAT(control_point_t[Dimension{0}], DoubleEq(2.1454));
 }
 
 TEST_F(AControlPoint, PerformsScalingTransformation) { // NOLINT
   std::array<double, 3> customScaling = {0.5, 1, 0.7};
-  baf::ControlPoint control_point_t = control_point_c.Transform(transMatrix, customScaling);
-  ASSERT_THAT(control_point_t.GetValueForDimension(Dimension{0}), DoubleNear(1.40178, 0.00001));
+  baf::ControlPoint control_point_t = control_point_c_.Transform(transformation_matrix_, customScaling);
+  ASSERT_THAT(control_point_t[Dimension{0}], DoubleNear(1.40178, 0.00001));
+}
+
+TEST_F(AControlPoint, ReturnsEuclideanNormOf) { // NOLINT
+  ASSERT_THAT(control_point_a_.GetEuclideanNorm(), DoubleEq(sqrt(5.0)));
 }
