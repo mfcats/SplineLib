c94c4640f2d958c699eeaa585dbc1f0503d3c4fb
christophsusen <christoph.susen@rwth-aachen.de>
2019-11-28 16:12:13 +0100

    Refactorization of ControlPoint finished.

--- test/spl/control_point_test.cc
+++ test/spl/control_point_test.cc
@@ -19,7 +19,6 @@ You should have received a copy of the GNU Lesser General Public License along w
 
 using testing::Test;
 using testing::DoubleEq;
-using testing::DoubleNear;
 
 using namespace splinelib::src;
 
